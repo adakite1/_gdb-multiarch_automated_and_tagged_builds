07242d3e8d20a19ad310902ff2fac7379b50cbbf
adakite1 <adakite.a@gmail.com>
2025-03-06 13:54:47 +0800

    Fix intel mac args, fix actions artifact names

--- mac/build-intel-mac-internal.sh
+++ mac/build-intel-mac-internal.sh
@@ -5,7 +5,6 @@ mkdir -p /tmp/build/gmp && cd /tmp/build/gmp && \
 	CFLAGS="-arch x86_64" CXXFLAGS="-arch x86_64" \
 	"/tmp/src/gmp-${GMP_VERSION}/configure" \
 		--prefix=/tmp/install/gmp \
-        --build=x86_64-apple-darwin \
         --host=x86_64-apple-darwin \
 		--enable-static \
 		--disable-shared
@@ -17,9 +16,8 @@ mkdir -p /tmp/build/mpfr && cd /tmp/build/mpfr && \
 	CFLAGS="-arch x86_64" CXXFLAGS="-arch x86_64" \
 	"/tmp/src/mpfr-${MPFR_VERSION}/configure" \
 		--prefix=/tmp/install/mpfr \
-        --build=x86_64-apple-darwin \
-        --host=x86_64-apple-darwin \
 		--with-gmp=/tmp/install/gmp \
+        --host=x86_64-apple-darwin \
 		--enable-static \
 		--disable-shared
 cd /tmp/build/mpfr && make "-j${CPU_CORES}"
@@ -30,10 +28,8 @@ mkdir -p /tmp/build/gdb && cd /tmp/build/gdb && \
 	CFLAGS="-O2 -g -arch x86_64" CXXFLAGS="-O2 -g -arch x86_64" \
 	"/tmp/src/gdb-${GDB_VERSION}/configure" \
 		--prefix=/tmp/install/gdb \
-        --build=x86_64-apple-darwin \
         --host=x86_64-apple-darwin \
 		--enable-targets=all \
-		--target=arm-none-eabi \
 		--with-gmp=/tmp/install/gmp \
 		--with-mpfr=/tmp/install/mpfr \
 		--with-static-standard-libraries \
@@ -51,6 +47,8 @@ cd /tmp/build/gdb && make install
 
 # Copy the GDB executables from the built files and strip away debug symbols to reduce the filesize
 mkdir /tmp/dist && \
-	cp /tmp/install/gdb/bin/arm-none-eabi-gdb /tmp/dist/gdb && \
-	strip /tmp/dist/gdb
+	cp /tmp/install/gdb/bin/gdb /tmp/dist/gdb && \
+	cp /tmp/install/gdb/bin/gdbserver /tmp/dist/gdbserver && \
+	strip -s /tmp/dist/gdb && \
+	strip -s /tmp/dist/gdbserver
 
