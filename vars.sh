009bdcfda1de3a9c4710177b6a5f37daa35fcf0e
adakite1 <adakite.a@gmail.com>
2026-03-08 17:54:04 +0800

    GDB 17.1

--- vars.sh
+++ vars.sh
@@ -1,6 +1,6 @@
 # The versions of GDB and libGMP that we will build
-export GDB_VERSION='16.2'
+export GDB_VERSION='17.1'
 export GMP_VERSION='6.3.0'
-export MPFR_VERSION='4.2.1'
-export EXPAT_VERSION='2.7.0'
+export MPFR_VERSION='4.2.2'
+export EXPAT_VERSION='2.7.4'
 
