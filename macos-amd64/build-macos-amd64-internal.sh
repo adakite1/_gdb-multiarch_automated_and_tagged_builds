8054635513ffd0e06368722f49d9c52d5297a434
adakite1 <adakite.a@gmail.com>
2026-03-09 09:00:17 +0800

    More flags

--- macos-amd64/build-macos-amd64-internal.sh
+++ macos-amd64/build-macos-amd64-internal.sh
@@ -2,7 +2,7 @@
 
 # Compile libGMP
 mkdir -p /tmp/build/gmp && cd /tmp/build/gmp && \
-	"/tmp/src/gmp-${GMP_VERSION}/configure" CC="gcc -m64" \
+	"/tmp/src/gmp-${GMP_VERSION}/configure" CC="cc -arch x86_64" \
 		--prefix=/tmp/install/gmp \
 		--enable-static \
 		--disable-shared \
@@ -12,7 +12,7 @@ cd /tmp/build/gmp && make install
 
 # Compile libMPFR
 mkdir -p /tmp/build/mpfr && cd /tmp/build/mpfr && \
-	"/tmp/src/mpfr-${MPFR_VERSION}/configure" CC="gcc -m64" \
+	"/tmp/src/mpfr-${MPFR_VERSION}/configure" CC="cc -arch x86_64" \
 		--prefix=/tmp/install/mpfr \
 		--with-gmp=/tmp/install/gmp \
 		--enable-static \
@@ -22,7 +22,7 @@ cd /tmp/build/mpfr && make install
 
 # Compile libexpat
 mkdir -p /tmp/build/expat && cd /tmp/build/expat && \
-	"/tmp/src/expat-${EXPAT_VERSION}/configure" CC="gcc -m64" \
+	"/tmp/src/expat-${EXPAT_VERSION}/configure" CC="cc -arch x86_64" \
 		--prefix=/tmp/install/expat \
 		--enable-static \
 		--disable-shared
@@ -37,7 +37,7 @@ tree /tmp/build
 # Compile GDB
 mkdir -p /tmp/build/gdb && cd /tmp/build/gdb && \
 	CFLAGS="-O2 -g" CXXFLAGS="-O2 -g" \
-	"/tmp/src/gdb-${GDB_VERSION}/configure" CC="gcc -m64" \
+	"/tmp/src/gdb-${GDB_VERSION}/configure" CC="cc -arch x86_64" CXX="c++ -arch x86_64" \
 		--prefix=/tmp/install/gdb \
 		--enable-targets=all \
 		--target=arm-none-eabi \
